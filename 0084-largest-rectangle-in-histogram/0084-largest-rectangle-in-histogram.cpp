c2d0add67f3ca66a50fd627cad8ca1621ee8cbfa
Harshmeet-singh06 <harshmitsingh2007@gmail.com>
2026-08-31 17:30:13 +0530

    Time: 23 ms (54.19%), Space: 81.4 MB (72.18%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
-    vector<int> next_smaller(vector<int>& a){
-        int n = a.size();
-        stack<int> st;
-        vector<int> greater(n);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && a[st.top()] >= a[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                greater[i] = n;
-            }
-            else{
-                greater[i] = st.top();
-            }
-            st.push(i);
-        }
-        return greater;
-    }
-    vector<int> prev_smaller(vector<int>& a){
-        int n = a.size();
-        stack<int> st;
-        vector<int> greater(n);
-        for(int i=0;i<n;i++){
-            while(!st.empty() && a[st.top()] >= a[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                greater[i] = -1;
-            }
-            else{
-                greater[i] = st.top();
-            }
-            st.push(i);
-        }
-        return greater;
-    }
     int largestRectangleArea(vector<int>& a) {
         int n = a.size();
-        vector<int> nse = next_smaller(a);
-        vector<int> pse = prev_smaller(a);
-        int area = 0;
+        stack<int> st;
+        int area = 0;   
         for(int i=0;i<n;i++){
-            int left = pse[i];
-            int right = nse[i];
-            int value = (right - left - 1) * a[i];
-            area = max(area,value);
+            while(!st.empty() && a[st.top()] > a[i]){
+                int element = a[st.top()];
+                st.pop();
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+                area = max(area,(nse - pse - 1) * element);
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int element = a[st.top()];
+            st.pop();
+            int nse = n;
+            int pse = st.empty() ? -1 : st.top();
+            area = max(area,(nse - pse -1) * element);
         }
         return area;
     }
